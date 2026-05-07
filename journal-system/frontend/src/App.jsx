c652384efde17bbee2ca3b33a4e811cb021b7787
Shivani Mohod <shivanimohod246@gmail.com>
2026-05-07 18:39:12 +0530

    Frontend updated

--- journal-system/frontend/src/App.jsx
+++ journal-system/frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Signup from "./pages/Signup";
 import Signin from "./pages/Signin";
 import Dashboard from "./pages/Dashboard";
@@ -7,6 +7,8 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/" element={<Navigate to="/signup" />} />
+
         <Route path="/signup" element={<Signup />} />
         <Route path="/signin" element={<Signin />} />
         <Route path="/dashboard" element={<Dashboard />} />
